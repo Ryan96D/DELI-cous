71fc5c0c4e41401d2d74dc7c48822fd024689b99
Ryan <doryan6996@gmail.com>
2025-05-28 01:43:01 -0700

    Update Order.java
    implements classes

--- DELI-cous/src/main/java/Models/Order.java
+++ DELI-cous/src/main/java/Models/Order.java
@@ -1,9 +1,26 @@
 package Models;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private List<Sandwich> sandwiches;
-    private List<Drink> drinks;
-    private List<Chips> chips;
+    private List<PricedItem> items = new ArrayList<>();
+
+    public void addItem(PricedItem item) {
+        items.add(item);
+    }
+
+    public BigDecimal getTotal() {
+        return items.stream()
+                .map(PricedItem::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public void printReceipt() {
+        for (PricedItem item : items) {
+            System.out.println(item.getReceiptLine());
+        }
+        System.out.println("Total: $" + getTotal());
+    }
 }
