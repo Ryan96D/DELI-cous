69b064eff655a600ea7c0899a0b1081f4adf2f6c
Ryan <doryan6996@gmail.com>
2025-05-28 01:41:53 -0700

    Update Chips.java

--- DELI-cous/src/main/java/Models/Chips.java
+++ DELI-cous/src/main/java/Models/Chips.java
@@ -2,6 +2,18 @@ package Models;
 
 import Enums.ChipType;
 
-public class Chips {
-    private ChipType chipType;
+import java.math.BigDecimal;
+
+public class Chips implements PricedItem {
+    private ChipType type;
+
+    @Override
+    public BigDecimal getPrice() {
+        return new BigDecimal("1.50");
+    }
+
+    @Override
+    public String getReceiptLine() {
+        return type + " Chips - $" + getPrice();
+    }
 }
