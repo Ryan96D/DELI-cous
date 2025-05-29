6164904d2fe1d176d6e964567354f79da0c6637a
Ryan <doryan6996@gmail.com>
2025-05-28 17:28:53 -0700

    Made generic class for Toppings

--- DELI-cous/src/main/java/models/items/ToppingSelection.java
+++ DELI-cous/src/main/java/models/items/ToppingSelection.java
@@ -1,4 +1,24 @@
 package models.items;
 
-public class ToppingSelection {
+public class ToppingSelection<T> {
+    private T topping;
+    private boolean isExtra;
+
+    public ToppingSelection(T topping, boolean isExtra) {
+        this.topping = topping;
+        this.isExtra = isExtra;
+    }
+
+    public T getTopping() {
+        return topping;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    @Override
+    public String toString() {
+        return topping.toString() + (isExtra ? " (extra)" : "");
+    }
 }
